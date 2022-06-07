d52b4d2be45fb2684e0a13565103856f749f6ed9
Venkateswaran <Suganya.Venkateswaran@agilysys.com>
2022-06-07 16:49:08 +0530

    updated CustomReporter

--- src/main/java/test/testNGAnnotationsSample.java
+++ src/main/java/test/testNGAnnotationsSample.java
@@ -1,41 +1,38 @@
 package test;
 
-
 import org.testng.annotations.*;
+import util.CustomReporter;
 
 @Listeners(CustomReporter.class)
 public class testNGAnnotationsSample {
 
     @BeforeTest
-    public void beforeTest(){
-
+    public void beforeTest() {
         System.out.println("Before Test");
     }
+
     @AfterTest
-    public void afterTest(){
-
+    public void afterTest() {
         System.out.println("After Test");
-
-}
+    }
 
     @BeforeMethod
-    public void beforeMethod(){
+    public void beforeMethod() {
         System.out.println("Before Methid");
     }
+
     @AfterMethod
-    public void afterMethod(){
+    public void afterMethod() {
         System.out.println("After Method");
     }
 
     @Test(groups = {"Smoke"})
-    public void Test1()
-    {
+    public void Test1() {
         System.out.println("Test 1");
     }
 
     @Test()
-    public void Test2()
-    {
-        System.out.println("Test 2" );
+    public void Test2() {
+        System.out.println("Test 2");
     }
 }
